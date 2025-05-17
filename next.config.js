953c7613bbc919dba7485d6f60659e73090e415d
laithessa1890 <qusayali0994@gmail.com>
2025-05-17 11:21:49 -0700

    fix: allow loading imagafassa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'elrqbhugwvvaguzqzvwm.supabase.co',
-        pathname: '/storage/v1/object/public/**',
-      },
-    ],
+    unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
